029258b48917cb0492bb7c5b1772b1dd14c8e9b9
empeje <11813607+empeje@users.noreply.github.com>
2023-04-07 23:58:03 +0200

    Add arm bitcoin installation

--- .install/bitcoin.sh
+++ .install/bitcoin.sh
@@ -3,6 +3,12 @@ then
     # URL to Bitcoin v22.0
     curl -o bitcoin.rb https://raw.githubusercontent.com/Homebrew/homebrew-core/fa6b4765d81016166f6de2bdad96cfe914c1439f/Formula/bitcoin.rb
     brew install ./bitcoin.rb
+elif [[ $(uname -m) == 'arm' ]]
+then
+    # URL to Bitcoin v22.0
+    curl -o bitcoin-22.0-arm-linux-gnueabihf.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-arm-linux-gnueabihf.tar.gz
+    tar xzf bitcoin-22.0-arm-linux-gnueabihf.tar.gz
+    sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-22.0/bin/*
 else
     curl -o bitcoin-22.0-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
     tar xzf bitcoin-22.0-x86_64-linux-gnu.tar.gz
