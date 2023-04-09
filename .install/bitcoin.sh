7bf784ea62e5caec5b4eed4b1a1975222e4972ff
empeje <11813607+empeje@users.noreply.github.com>
2023-04-09 20:51:28 +0200

    Fix bitcoin

--- .install/bitcoin.sh
+++ .install/bitcoin.sh
@@ -26,7 +26,7 @@ then
     curl -o bitcoin.rb https://raw.githubusercontent.com/Homebrew/homebrew-core/fa6b4765d81016166f6de2bdad96cfe914c1439f/Formula/bitcoin.rb
     brew install ./bitcoin.rb
 else
-    curl -o bitcoin-22.0-${platform}.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-${platform}.gz
+    curl -o bitcoin-22.0-${platform}.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-${platform}.tar.gz
     tar xzf bitcoin-22.0-${platform}.tar.gz
     sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-22.0/bin/*
 fi
